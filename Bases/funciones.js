b6d39fc61e18dd453b1d025402598ea36fd0a9e2
avr_am <aacunamarcos@gmail.com>
2021-03-07 12:45:31 +0100

    7/3/21

---
+++ Bases/funciones.js
@@ -0,0 +1,11 @@
+function result(number){
+    if(number % 2 == 0){
+        return true
+    }
+    return false
+
+}
+
+var number = 22
+
+console.log(result(number))
---
+++ Bases/variables.js
@@ -0,0 +1,12 @@
+var names = 'Alvaro'
+
+var age = 16
+
+var car = {
+    marca : 'Ford',
+    matricula : 'UYC1579'
+}
+
+var total = names + ' ' + age + ' ' + car.marca + ' ' + car.matricula
+console.log(total)
+
