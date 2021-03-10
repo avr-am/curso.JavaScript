fc57f86d1c74b3846f54bd3849c341563e10170e
avr_am <aacunamarcos@gmail.com>
2021-03-10 17:29:55 +0100

    10/3/21

---
+++ Bases/Cliclos/ciclo_for.js
@@ -0,0 +1,3 @@
+for(var i = 0;i<10;i++){   //inicialización, condición e incremento
+    
+}
---
+++ Bases/Cliclos/ciclo_while.js
@@ -0,0 +1,17 @@
+var contador = 0
+
+while(contador < 10){
+    contador = contador + 1 //es lo mismo poner contador++
+    if(contador % 2 == 0){
+    console.log(contador)
+    }else{
+        continue
+    }
+}
+
+
+
+/*
+break
+continue
+*/
